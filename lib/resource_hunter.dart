d9367f3f3b614aef0644ff02457b987c9b61a14f
lhlyu <misakafs@gmail.com>
2024-08-19 22:22:39 +0800

    update

--- lib/resource_hunter.dart
+++ lib/resource_hunter.dart
@@ -5,6 +5,7 @@ export 'src/v/parse/parser.dart' show VideoParseRequest, VideoParseResponse;
 export 'src/v/platforms.dart' show VideoPlatforms;
 export 'src/v/query/querier.dart' show VideoQueryRequest, VideoQueryResponse, VideoQueryItem;
 export 'src/v/search/searcher.dart' show VideoSearchRequest, VideoSearchResponse, VideoSearchItem;
+export 'src/v/v_platform.dart' show VChannel;
 export 'src/v/v_tool.dart' show VTool;
 export 'src/v/video_platform.dart' show VideoPlatform;
 export 'src/v/view/viewer.dart' show VideoViewRequest, VideoViewResponse, VideoViewItem;
